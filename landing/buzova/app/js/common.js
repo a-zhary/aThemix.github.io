ec3137277a884f19fee649ca9dd65b8457165627
Anton Zhary <anton.zhary@gmail.com>
2019-12-23 14:45:37 +0300

    Buzova landing

---
+++ landing/buzova/app/js/common.js
@@ -0,0 +1,16 @@
+$(function () {
+
+	$('.audio-slider').slick({
+		fade: true,
+		// slidesToShow: 1,
+		// slidesToScroll: 1,
+		// // asNavFor: '.slider-for',
+		// dots: true,
+		// // centerMode: true,
+		// // focusOnSelect: true,
+		// variableWidth: true
+	});
+
+	new WOW().init();
+
+});
